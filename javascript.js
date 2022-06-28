2188b332da59704e806ada874a2a2d4ff77756f2
Janni <36205116+Janniii@users.noreply.github.com>
2022-06-28 21:45:17 +0200

    Add trail-feature and ability to change color of column borders

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 
 let selection = "white";
 
+let redValue = Math.floor(Math.random() * 256);
+let greenValue = Math.floor(Math.random() * 256);
+let blueValue = Math.floor(Math.random() * 256);
+let opacityValue = 0.0;
+
+let selectionTrail = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${opacityValue})`;
+
 
 let currentColors;
 
@@ -43,10 +50,14 @@ const colorListReverse  = {
 
 
 
+
+
+
 const container = document.querySelector("#container");
 
 
 function createGrid(n) {
+
 
     for (let i = 0; i < n; i++) {
       const columnDiv = document.createElement("div");
@@ -87,50 +98,110 @@ rows.forEach(row => row.addEventListener("click", () => {
 
 function hover() {
     this.style.backgroundColor = selection;
-
 }
 
 
 
-const hov = document.querySelector(".hover");
 
+function trailHover() {
+  // selection =`rgba(${redValue}, ${greenValue}, ${blueValue}, ${opacityValue+=0.1})`
+  selectionTrail = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${opacityValue+=0.1})`
+  //this.style.backgroundColor = selection;
+  this.style.backgroundColor = selectionTrail;
+
+  if (opacityValue.toFixed(1) >= 1.0) {
+    redValue = Math.floor(Math.random() * 256);
+    greenValue = Math.floor(Math.random() * 256);
+    blueValue = Math.floor(Math.random() * 256);
+    opacityValue = 0.0;
+
+  }
+
+
+
+  console.log(selection);
+  console.log(opacityValue);
+}
+
+
+
+// const hov = document.querySelector(".hover");
+
+const hov = document.querySelector("#Hover");
+const light = document.querySelector("#Light");
+const trail = document.querySelector("#Trail");
 
 
 let hoverToggle = 0;
 let lightToggle = 0;
+let trailToggle = 0;
 
 window.addEventListener("keydown", (e) => {
+    const rows = document.querySelectorAll(".row");
+
+    if (trailToggle == 0) {
+
+
     if (hoverToggle == 0 && e.keyCode == 72) {
       hoverToggle = 1;
+      hov.classList.add("select2");
       rows.forEach(row => row.addEventListener("mouseover", hover))
     }
       else if (hoverToggle != 0 && e.keyCode == 72) {
         hoverToggle = 0;
+        hov.classList.remove("select2");
         rows.forEach(row => row.removeEventListener("mouseover", hover))
       }
 
     
     if (lightToggle == 0 && e.keyCode == 76) {
       lightToggle = 1;
+      light.classList.add("select2");
       previous = colorList[previous];
       selection = colorList[selection];
-
-      
 
       }
 
      
-
-    
       else if (lightToggle != 0 && e.keyCode == 76) {
         lightToggle = 0;
+        light.classList.remove("select2");
         previous = colorListReverse[previous];
         selection = colorListReverse[selection];
-        
-      
        
       }
-    });
+    }
+  
+
+    if (trailToggle == 0 && e.keyCode == 84) {
+      hov.classList.remove("select2");;
+      light.classList.remove("select2");
+
+      // buttons.forEach(button => button.classList.remove("select"));
+
+      trail.classList.add("select2");
+      trailToggle = 1;
+      // selection = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${opacityValue})`
+      selectionTrail = `rgba(${redValue}, ${greenValue}, ${blueValue}, ${opacityValue})`
+      rows.forEach(row => row.addEventListener("mouseover", trailHover))
+
+    }
+
+      else if (trailToggle != 0 && e.keyCode == 84) {
+        trail.classList.remove("select2")
+        rows.forEach(row => row.removeEventListener("mouseover", trailHover))
+        trailToggle = 0;
+      }
+
+
+    
+
+  
+
+
+
+  
+   });
 
 
 const buttons = document.querySelectorAll(".buttongroup button");
@@ -152,6 +223,7 @@ let previous = whiteButton.id;
 
 
 function selected() {
+  
 
   if (lightToggle == 0 && previous != this.id) {
     let prev = document.getElementById(previous);
@@ -171,6 +243,73 @@ function selected() {
 
 this.classList.add("select");
 
+
+
+}
+
+
+
+const toolButtons = document.querySelectorAll(".whoops .tooltip");
+
+toolButtons.forEach(toolButton => toolButton.addEventListener("click", getFunctionality))
+
+
+
+function getFunctionality() {
+  
+  if (this.id == "Grid") {
+    while (true) {
+    let gSize = prompt("Please Input a number between 1 and 100 to generate a new grid.")
+
+    if (gSize == null) {
+      break;
+    }
+
+    else if (parseInt(gSize) >= 1 && parseInt(gSize) <= 100) {
+
+      const coll = document.querySelectorAll(".column");
+      const roww = document.querySelectorAll(".row");
+
+      coll.forEach(col => col.remove());
+      roww.forEach(row => row.remove())
+
+
+
+      gSize = parseInt(gSize)
+      createGrid(gSize);
+      reGrid()
+      break;
+    }
+
+
+    }
+  }
+
+  if (this.id == "Tiles") {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => row.style.backgroundColor = selection);
+  }
+
+  if (this.id == "Border") {
+    const rows = document.querySelectorAll(".row");
+    rows.forEach(row => row.style.borderColor = selection);
+  }
+}
+
+
+
+
+function reGrid () {
+
+
+  const rows = document.querySelectorAll(".row");
+  rows.forEach(row => row.addEventListener("click", () => {
+    row.style.backgroundColor = `${selection}`
+    }))
+  hov.classList.remove("select2")
+  light.classList.remove("select2")
+  hoverToggle = 0;
+  lightToggle = 0;
 
 
 }
